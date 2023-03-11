4e8b53cd663d7af458826380c0adbbddff54f612
Alonso Baldioceda Saborio <alonso.baldioceda@gmail.com>
2023-03-11 16:19:35 -0600

    Testing deploy

--- .storybook/main.js
+++ .storybook/main.js
@@ -47,4 +47,14 @@ module.exports = {
     options.plugins.push("babel-plugin-inline-react-svg");
     return options;
   },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
